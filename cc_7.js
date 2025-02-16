f3bcea28b217aad791faecb83e6986182ca54a20
Derek Ruize <derekruiz@usf.edu>
2025-02-16 14:33:21 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,13 @@ function calculateInvoice(subtotal, taxRate, discount) {
 
 calculateInvoice(100, 0.08, 5); 
 calculateInvoice(500, 0.1, 20); 
+
+//Task 2 - Function Expression
+const calculateHourlyWage = function (salary, hoursPerWeek) {
+    hourlyWage = salary / (hoursPerWeek * 52) //Formula for calculating hourly wage
+    let rounded = hourlyWage.toFixed(2); //Rounding so it stops at two decimal points
+    console.log("Hourly Wage:", "$" + rounded)
+};
+
+calculateHourlyWage(52000, 40); 
+calculateHourlyWage(75000, 35); 
